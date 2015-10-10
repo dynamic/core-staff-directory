716cd6c646e1575d6c3313f23f38347b3288d8f2
Jason Irish <jirish@dynamicagency.com>
2015-10-10 15:33:41 -0500

    Field and formatting cleanup

--- code/Staff.php
+++ code/Staff.php
@@ -1,66 +1,44 @@
 <?php
 
-class Staff extends DetailPage {
+class Staff extends DetailPage
+{
+    private static $singular_name = 'Staff Member';
+    private static $plural_name = 'Staff Members';
+    private static $description = 'Profile of a staff member';
 
-    private static $singular_name = 'Staff';
-    private static $plural_name = 'Staffs';
-    private static $description = 'Staff member';
+    private static $db = array(
+        'JobTitle' => 'Varchar(255)',
+        'Email' => 'Varchar(255)',
+    );
 
-	private static $db = array(
-		'JobTitle'	=> 'Varchar(255)',
-		'Email' => 'Varchar(255)'
-	);
-	
-	private static $has_one = array();
-	
-	private static $many_many = array(
-
-	);
-	
-	private static $belongs_many_many = array(
-
-	);
-	
-	private static $summary_fields = array(
+    private static $summary_fields = array(
         'Title',
-		'JobTitle',
-		'Email'
-	);
-	
-	//CMS fields
-    function getCMSFields()
+        'JobTitle',
+        'Email',
+    );
+
+    //CMS fields
+    public function getCMSFields()
     {
         $fields = parent::getCMSFields();
-        
+
         // remove sort order, use gridfield drag and drop instead
         $fields->removeByName('SortOrder');
-        
-        // Image - custom upload
-		/*$ImageField = new UploadField('Image', 'Image');
-		$ImageField->getValidator()->allowedExtensions = array('jpg', 'gif', 'png');
-		$ImageField->setFolderName('Uploads/Staff');*/
-		//$fields->addFieldToTab('Root.Images', $ImageField);
-        
-        $fields->addFieldsToTab(
-            'Root.Main',
+
+        $fields->addFieldsToTab('Root.Main',
             array(
-                TextField::create('JobTitle')
-                    ->setTitle('Title'),
-                EmailField::create('Email')
-                    ->setTitle('Email')
+                TextField::create('JobTitle', 'Title'),
+                EmailField::create('Email'),
             ),
             'Content'
         );
 
         $this->extend('updateCMSFields', $fields);
-         
+
         return $fields;
     }
-    
 }
 
-class Staff_Controller extends DetailPage_Controller{
-
-
-
+class Staff_Controller extends DetailPage_Controller
+{
 }
